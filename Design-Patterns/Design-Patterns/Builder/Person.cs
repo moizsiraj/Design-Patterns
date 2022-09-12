c49b642507d988a1ac6727597b3b073662dc96ee
Moiz Siraj <abdulmoiz68@gmail.com>
2022-09-13 00:36:52 +0500

    driver files

---
+++ Design-Patterns/Design-Patterns/Builder/Person.cs
@@ -0,0 +1,22 @@
+﻿using Design_Patterns.Builder.Fluent_Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Builder
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }    
+        public string Designation { get; set; }
+        public string Company { get; set; }
+        public long Salary { get; set; }
+
+        public static PersonBuilder New => new PersonBuilder();
+    }
+}
--- Design-Patterns/Design-Patterns/Program.cs
+++ Design-Patterns/Design-Patterns/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Design_Patterns.Builder;
+using Design_Patterns.Builder.Builder_Facade;
+
+var fluentBuilder = Person.New;
+fluentBuilder.WorksAt("XYZ").LivesOn("Street 4").WorksAsA("Software Engineer");
+
+var facadeBuilder = new PersonBuilder();
+facadeBuilder.Works.At("XYZ")
+             .Lives.In("Karachi")
+             .Works.AsA("Software Engineer")
+             .Lives.On("Street 4");
+
